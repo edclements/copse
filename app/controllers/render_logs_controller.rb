1911b1603a52afeb4a857186c12b7985c49692eb
Ed Clements <ed@bookingbug.com>
2012-08-22 13:25:57 +0100

    set default limit for render log index

--- app/controllers/render_logs_controller.rb
+++ app/controllers/render_logs_controller.rb
@@ -3,7 +3,9 @@ class RenderLogsController < ApplicationController
   def index
     action = params[:selected_action]
     controller = params[:selected_controller]
-    @logs = RenderLog.where('action = ? and controller = ?', action, controller)
+    limit = params[:limit]
+    limit ||= 50
+    @logs = RenderLog.where('action = ? and controller = ?', action, controller).limit(limit)
     render :json => @logs.to_json
   end
 
